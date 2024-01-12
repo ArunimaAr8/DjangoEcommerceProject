c8666dc7d2d38aee09a1e9d6250dc6f8a1db4ba6
ArunimaAr8 <154635525+ArunimaAr8@users.noreply.github.com>
2024-01-12 11:06:49 -0500

    Updated views.py

--- customer/views.py
+++ customer/views.py
@@ -45,8 +45,8 @@ class CustomerLogin(APIView):
             if serializer.is_valid():
                 username = serializer.data["username"]
                 password = serializer.data["password"]
-                loginData = list(CustomerData.objects.filter(username=username, password=password).values())
-                if len(loginData) == 1:
+                logindata = list(CustomerData.objects.filter(username_id=username, password=password).values())
+                if len(logindata) == 1:
                     message = {"Message": "Login Successful"}
                     return JsonResponse(message, status=status.HTTP_200_OK)
                 else:
